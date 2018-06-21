59bede8de14dea85070ca045e39dadf8613618d0
SimonDeussen <simon.deussen@gmail.com>
2018-06-21 10:48:14 +0200

    added code

---
+++ main.js
@@ -0,0 +1,316 @@
+const {app, BrowserWindow, protocol, ipcMain} = require('electron')
+const path = require('path')
+const url = require('url')
+let myPort = 1337;
+const windowName = "main"
+
+let debug = false;
+let fullscreen = false;
+let browserAlive = false;
+let isConnected = false;
+
+// taking care of commandline args
+var arguments = process.argv.slice(2);
+arguments.forEach(function(value,index, array) {
+  let splittedArg = value.split("=");
+
+  if ("fullscreen" == splittedArg[0] || "fs" == splittedArg[0])
+  {
+    fullscreen = true;
+    console.log("app is now running in fullscreen mode");
+  }
+
+  if ("debug" == splittedArg[0])
+  {
+    debug = true;
+    console.log("app is now running in debug mode");
+  }
+
+  if ("port" == splittedArg[0])
+  {
+    myPort = splittedArg[1]
+    console.log("app is now running with different port " + myPort);
+  }
+});
+
+// für ipc mit python
+let net = require('net');
+let client = new net.Socket();
+
+let win
+
+function createWindow () {
+  win = new BrowserWindow({width: 1800, height: 1012, title: "Advanced AI stuff or not..."})
+
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true,
+    name: windowName
+  }))
+
+  if (debug)
+  {
+    win.webContents.openDevTools();
+  }
+
+  if (fullscreen)
+  {
+    win.setFullScreen(true)
+  }
+
+
+  win.on('closed', () => {
+    win = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow()
+  }
+})
+
+
+let greetingManager = {
+  sessions: new Map(),
+  activeSession: undefined,
+  interval: undefined,
+
+  addData: function(name, area, position)
+  {
+    console.log(name, area);
+
+    if (browserAlive && this.sessions.size == 0)
+    {
+        this.sessions.set(name, this.createNewEntry(name, area, position));
+        console.log("cleared interval, found new dude");
+        clearInterval(this.interval);
+        this.startInterval();
+    }
+    if (!this.sessions.has(name))
+    {
+        this.sessions.set(name, this.createNewEntry(name, area, position));
+        console.log("creating new entry for " + name);
+    }
+    else
+    {
+
+      // console.log("updating existing entry");
+      let existingEntry = this.sessions.get(name);
+
+      clearTimeout(existingEntry.timeout);
+
+      existingEntry.timeout = setTimeout(function() {
+        greetingManager.deleteEntry(name)
+      }, 1500);
+
+      existingEntry.area = area;
+      existingEntry.position = position;
+
+    }
+  },
+
+  createNewEntry: function(name,area,position)
+  {
+    return {
+      "name": name,
+      "timeout": setTimeout(function() {
+        greetingManager.deleteEntry(name)
+      }, 1000),
+      "area": area,
+      "position": position
+    }
+  },
+
+  deleteEntry: function(name)
+  {
+    console.log("deleting entry " + name);
+    console.log("");
+    let sessionName = this.sessions.get(name).name;
+    let activeName;
+    if (this.activeSession != undefined && this.activeSession.hasOwnProperty("name"))
+    {
+      activeName = this.activeSession.name
+    }
+    else {
+      activeName = undefined;
+      console.log("+++++++++++++++++++++++++++++++++++++++++++ already deleted");
+    }
+    if (activeName != undefined && sessionName == activeName)
+    {
+      this.activeSession = undefined;
+    }
+
+    this.sessions.delete(name);
+    win.webContents.send('stop-session' , {"name": name});
+    // this.checkIfThereAreMoreFaces();
+
+  },
+
+  getSessionWithBiggestArea: function()
+  {
+    let keys = Array.from( this.sessions.keys() );
+    let biggest;
+
+    keys.forEach( key => {
+      if (biggest == null)
+      {
+        biggest = this.sessions.get(key);
+      }
+      else
+      {
+        if (this.sessions.get(key).area >= biggest.area)
+          biggest = this.sessions.get(key);
+      }
+    });
+    return biggest;
+  },
+
+  sendNewSession: function()
+  {
+    let biggestSession = this.getSessionWithBiggestArea();
+    if (biggestSession != null)
+    {
+      if (this.activeSession == null || this.activeSession.name != biggestSession.name)
+      {
+        this.activeSession = biggestSession;
+        console.log("biggest session " + this.activeSession.name + " " + this.activeSession.area);
+        console.log("sending new session");
+        win.webContents.send('new-session' , {"name": this.activeSession.name });
+      }
+    }
+    else
+    {
+      console.log("No session found");
+    }
+  },
+
+  checkIfThereAreMoreFaces: function()
+  {
+    if(this.sessions.size > 0)
+    {
+      this.sendNewSession();
+    }
+  },
+
+  startInterval: function()
+  {
+    if (isConnected)
+    {
+      console.log("starting up, sending new session");
+      this.sendNewSession();
+
+      interval = setInterval( () => {
+        console.log("interval");
+        if (this.sessions.size > 0)
+        {
+          greetingManager.sendNewSession();
+        }
+      }, 3000)
+    }
+    else
+    {
+      console.log("no conncection to backend, not starting interval");
+      console.log("frontend runs in dummy mode");
+
+      setTimeout(function () {
+        console.log(1);
+        win.webContents.send('stop-session' , {"name": "test2"});
+      },200);
+      setTimeout(function () {
+        console.log(2);
+        win.webContents.send('new-session' , {"name": "test1" });
+      }, 300);
+      setTimeout(function () {
+        console.log(3);
+        win.webContents.send('stop-session' , {"name": "test1"});
+
+      }, 400);
+      setTimeout(function () {
+        console.log(4);
+        win.webContents.send('new-session' , {"name": "test2" });
+
+      }, 500);
+
+
+      // creates weird in between state
+      // setTimeout(function () {
+      //   console.log(9);
+      //   win.webContents.send('stop-session' , {"name": "test1"});
+      //   win.webContents.send('new-session' , {"name": "test2" });
+      //   win.webContents.send('stop-session' , {"name": "test2"});
+      //   win.webContents.send('stop-session' , {"name": "test3"});
+      //   win.webContents.send('stop-session' , {"name": "test4"});
+      //   win.webContents.send('new-session' , {"name": "test2" });
+      //   win.webContents.send('new-session' , {"name": "test1" });
+      // }, 400);
+      // setTimeout(function () {
+      //   console.log(10);
+      //   win.webContents.send('stop-session' , {"name": "test1"});
+      //   win.webContents.send('stop-session' , {"name": "test3"});
+      //   win.webContents.send('stop-session' , {"name": "test2"});
+      //   win.webContents.send('new-session' , {"name": "test2" });
+      //   win.webContents.send('new-session' , {"name": "test2" });
+      //   win.webContents.send('stop-session' , {"name": "test4"});
+      //   win.webContents.send('new-session' , {"name": "test1" });
+      // }, 490);
+    }
+  }
+}
+
+// IPC mit python
+client.connect(myPort, '127.0.0.1', function() {
+  isConnected = true;
+	console.log('Connected');
+});
+
+client.on('data', function(data) {
+
+  let message = data.toString();
+  let splittedMessage = message.split(";");
+
+  splittedMessage.forEach( value => {
+    if (value != "")
+    {
+      let entry = JSON.parse(value) 
+      greetingManager.addData(entry.name, entry.area, entry.position);
+      win.webContents.send('position' , entry.position);
+    }
+  });
+
+});
+
+client.on('error', function(error) {
+  if(error.code == "ECONNREFUSED")
+  {
+    console.log("connection refused - pyserver not started?");
+  }
+  else
+  {
+    console.log(error);
+  }
+  isConnected = false;
+  client.destroy();
+});
+
+client.on('close', function() {
+  isConnected = false;
+	console.log('Connection closed');
+});
+
+// teste die verbindung zum browser window
+ipcMain.on('asynchronous-message', (event, arg) => {
+  console.log(arg) // prints "ping"
+  event.sender.send('asynchronous-reply', 'hello from electron')
+  browserAlive = true;
+  greetingManager.startInterval();
+})
---
+++ public/script.js
@@ -0,0 +1,428 @@
+const {ipcRenderer} = require('electron')
+
+
+
+let greetingInProcess = false
+let currentPerson;
+let deletionTimeout;
+
+let parent = getEl(".inner")[0];
+let hello = document.createElement("h1");
+let extraText = document.createElement("h2");
+parent.appendChild(hello);
+parent.appendChild(extraText);
+
+
+// TESTS
+console.log("Hello world")
+// teste die verbindung zum electron
+ipcRenderer.on('asynchronous-reply', (event, arg) => {
+  console.log(arg)
+});
+
+ipcRenderer.send('asynchronous-message', 'hello from frontend');
+
+particlesJS.load('particles-js', 'public/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
+
+
+function setPositionForParticleEffect(x,y) {
+  window.pJSDom[0].pJS.interactivity.mouse.click_pos_x = 1000
+  window.pJSDom[0].pJS.interactivity.mouse.click_pos_y = 500
+}
+
+function setModeForParticleEffect(mode) {
+  const modes = ["push", "repulse", "bubble"]
+  if (modes.includes(mode)) {
+    window.pJSDom[0].pJS.interactivity.events.onclick.mode = mode
+  } else {
+    throw new Error("Invalid mode " + mode)
+  }
+}
+
+function dispatchFakeMouseEventForParticleJs(type, x, y) {
+  var particles = getEl('.particles-js-canvas-el')[0];
+  var evt = mouseEvent(type, x, y, x, y);
+ 
+  dispatchEvent(particles, evt);
+}
+
+function triggerParticlePush(count,x,y) {
+  try {
+    setModeForParticleEffect("push")
+  } catch (error) {
+    console.log(error.message)
+    return
+  }
+
+  window.pJSDom[0].pJS.fn.modes.pushParticles(count, {"pos_x": x, "pos_y": y})
+  
+  console.log("triggering push at", x, y)
+}
+
+function triggerParticleBubble(x,y) {
+  try {
+    setModeForParticleEffect("bubble")
+  } catch (error) {
+    console.log(error.message)
+    return
+  }
+
+  dispatchFakeMouseEventForParticleJs("mousemove", x,y)
+  console.log("triggering push at", x, y)
+}
+
+function triggerParticleRepulse(x,y) {
+  try {
+    setModeForParticleEffect("repulse")
+  } catch (error) {
+    console.log(error.message)
+    return
+  }
+
+  setPositionForParticleEffect(x,y)
+  dispatchFakeMouseEventForParticleJs("click")
+
+  console.log("triggering repulse at", x, y)
+}
+
+function setParticleOpacity(opacity) {
+  window.pJSDom[0].pJS.particles.opacity.value = opacity;  
+}
+
+
+function getGreetingText()
+{
+  let oneDigit = () => Math.floor(Math.random() * 10)
+
+  let directions;
+
+  let key = Math.floor(Math.random() * 3)
+  switch (key) {
+    case 0:
+      directions = "please follow the staircase on the left."
+      break;
+    case 1:
+      directions = "please go straight into the hallway."
+      break;
+    case 2:
+        directions = "please follow along on the right."
+        break;
+    default:
+      console.log("WTF?? " + key);
+  }
+
+  let roomNumber = "A" + oneDigit() + oneDigit() + oneDigit();
+
+  return "Your upcoming meeting is in " + roomNumber + ", " + directions;
+}
+
+function startNewGreeting(name)
+{
+  if (greetingInProcess)
+  {
+    console.log("already greeting someone else....")
+  }
+  else
+  {
+    greetingInProcess = true;
+    currentPerson = name;
+    clearTimeout(deletionTimeout);
+
+    getEl(".outer-square")[0].classList.remove("rotate")
+    getEl(".inner-square")[0].classList.add("whiter")
+    getEl("#text")[0].classList.add("invisible");
+
+    hello.textContent = "Hello " + name.charAt(0).toUpperCase() + name.slice(1) + "!";
+    hello.setAttribute("id", "headline")
+    setTimeout(function () {
+      hello.classList.add("make-visible")
+    }, 0);
+
+    setTimeout(function() {
+      extraText.textContent = getGreetingText();
+      extraText.setAttribute("id", "extratext");
+      setTimeout(function () {
+        extraText.classList.add("make-visible")
+      }, 0);
+    }, 500)
+
+    getEl(".wrapper")[0].classList.add("start-bg-animation");
+  }
+}
+
+function deleteGreeting() {
+  console.log("shutting down last greeting");
+
+  let hello = getEl("#headline")[0]
+  let extraText = getEl("#extratext")[0]
+
+  getEl(".outer-square")[0].classList.add("rotate");
+  getEl(".inner-square")[0].classList.remove("whiter")
+  getEl("#text")[0].classList.remove("invisible");
+  getEl(".wrapper")[0].classList.remove("start-bg-animation");
+
+  if (hello != null && extraText != null)
+  {
+    hello.classList.remove("make-visible")
+    setTimeout(() => { extraText.classList.remove("make-visible") }, 500);
+
+  }
+
+  deletionTimeout = setTimeout(function () {
+    let hello1 = getEl("#headline")[0]
+    let extraText1 = getEl("#extratext")[0]
+    if (hello1 != null && extraText1 != null)
+    {
+      hello1.textContent = "";
+      extraText1.textContent = "";
+
+    }
+  }, 1000);
+
+  greetingInProcess = false;
+  currentPerson == undefined;
+}
+
+let interpolator = {
+  interpolate: function (start, stop, count) {
+    let difference = Math.abs(stop - start)
+    let positive = (stop - start) > 0
+    let stepsize = difference / count
+    let out = []
+
+    // TODO IMPLEMENT BACKWARDS COUNTING!
+    for (let i = start; i <= stop; ) {
+      out.push(i)
+
+      if (positive) {
+        i = i + stepsize
+      } else {
+        i = i - stepsize
+      }
+    }
+
+    return out
+  },
+
+  currentState : {empty: true, last: "", interpolatedPositions: "", next: "", interval: "", interpolationCount: 0},
+
+  calculatePosition: function(x0, y0, x1, y1) {
+    let middle_x = (x0 + x1) / 2 
+    let middle_y = (y0 + y1) / 2
+    
+    let particleCanvas = getEl("#particles-js > canvas")[0]
+  
+    let scaled_pos_x = particleCanvas.getAttribute("width") - middle_x / 320 * particleCanvas.getAttribute("width")
+    let scaled_pos_y = middle_y / 240 * particleCanvas.getAttribute("height")
+
+    return { x: scaled_pos_x, y: scaled_pos_y}
+  },
+
+  updateState: function(x0, y0, x1, y1) {
+    let correctedPosition = this.calculatePosition(x0, y0, x1, y1)
+    let particleCanvas = getEl("#particles-js > canvas")[0]
+
+    if (this.currentState.empty) {
+      console.log("starting new interpolation session")
+
+      last = {x: particleCanvas.getAttribute("width") / 2, y: particleCanvas.getAttribute("height") / 2}
+      console.log("LAST", last)
+      next = {x: correctedPosition.x, y: correctedPosition.y}
+      console.log("NEXT", next)
+      interpolatedPositions = {x: this.interpolate(last.x, next.x, 10), y: this.interpolate(last.y, next.y, 10)}
+
+      console.log(last, next)
+      console.log(interpolatedPositions);
+      
+
+
+    } else {
+      console.log("updating interpolation session")
+      
+    }
+  }
+}
+
+setTimeout(() => {
+  console.log("Testing Interpolation")
+  console.log(interpolator.interpolate(1,10,9)) 
+
+}, 5000);
+
+
+ipcRenderer.on("position", (event, arg) => {
+  let correctedPosition = interpolator.calculatePosition(arg.x0, arg.y0, arg.x1, arg.y1)
+  triggerParticleBubble(correctedPosition.x, correctedPosition.y)
+
+  interpolator.updateState(arg.x0, arg.y0, arg.x1, arg.y1)
+})
+
+// function addNewFacePosition(newPosition) {
+//   facePositions.last = facePositions.current
+//   facePositions.current = newPosition
+//   interpolationCount = 0
+// }
+
+
+
+// let particleCanvas
+// let canvasWidth
+// let canvasHeight
+
+// setTimeout(() => {
+//   particleCanvas = getEl("#particles-js > canvas")[0]
+//   canvasWidth = particleCanvas.getAttribute("width")
+//   canvasHeight = particleCanvas.getAttribute("height")
+// }, 300);
+
+
+// let facePositions = {"last": undefined, "current": undefined}
+// let interpolationCount = 0
+// let interpolationInterval = undefined
+// let killTimeout = undefined
+// let updateRate = 50
+// let interpolationIntervalStarted = false
+
+// function startNewFaceInterval(position) {
+
+//   killTimeout = setTimeout(() => {
+//     clearInterval(interpolationInterval)
+//   }, 1000);
+
+//   interpolationIntervalStarted = true
+//   facePositions.current = position
+//   facePositions.last = {"x": canvasWidth/2, "y": canvasHeight/2}
+
+//   interpolationInterval = setInterval(() => {
+//     let finalX, finalY
+//     if (facePositions.last != undefined && facePositions.current != undefined) {
+//       let distanceX = facePositions["current"]["x"] - facePositions["last"]["x"]
+//       let distanceY = facePositions["current"]["y"] - facePositions["last"]["y"]
+      
+//       finalX = facePositions["last"]["x"] + interpolationCount * distanceX / updateRate
+//       finaly = facePositions["last"]["y"] + interpolationCount * distanceY / updateRate
+      
+//       if (interpolationCount < updateRate) {
+//         console.log("updated interpolation count", interpolationCount)
+//         interpolationCount = interpolationCount + 1
+//       } else {
+//         console.log("clearing interval because no update")
+//         clearInterval(interpolationInterval)
+//         interpolationIntervalStarted = false
+
+//       }
+
+//       triggerParticleBubble(finalX, finalY)
+
+//     }
+
+//   }, 1000/ updateRate)
+// }
+
+
+// function addPositionParticleEffect(position) {
+//   let middle_x = (position.x0 + position.x1) / 2 
+//   let middle_y = (position.y0 + position.y1) / 2
+  
+//   particleCanvas = getEl("#particles-js > canvas")[0]
+
+//   let scaled_pos_x = particleCanvas.getAttribute("width") - middle_x / 320 * particleCanvas.getAttribute("width")
+//   let scaled_pos_y = middle_y / 240 * particleCanvas.getAttribute("height")
+
+
+//   if (!interpolationIntervalStarted) {
+//     startNewFaceInterval({"x": scaled_pos_x, "y": scaled_pos_y})
+//   } else {
+//     addNewFacePosition({"x": scaled_pos_x, "y": scaled_pos_y})
+//   }
+// }
+
+ipcRenderer.on("new-session", (event, arg) => {
+  console.log(arg.name);
+  console.log(arg)
+
+  if (greetingInProcess)
+  {
+    console.log("another greeting in process");
+    deleteGreeting()
+    setTimeout(function () {
+      startNewGreeting(arg.name);
+    }, 500);
+  }
+  else
+  {
+    startNewGreeting(arg.name);
+  }
+});
+
+ipcRenderer.on("stop-session", (event, arg) => {
+  if (arg != null && arg.hasOwnProperty("name"))
+  {
+    if (arg.name == currentPerson)
+    {
+      console.log("Stopping session " + arg.name);
+      deleteGreeting();
+    }
+    else
+    {
+      console.log("wanting to delete person not shown");
+    }
+  }
+  else {
+    console.log("got the fucking error");
+    console.log(arg);
+  }
+})
+
+
+
+function getEl(sel)
+{
+  return document.querySelectorAll(sel)
+}
+
+function mouseEvent(type, sx, sy, cx, cy) {
+  var evt;
+  var e = {
+    bubbles: true,
+    cancelable: (type != "mousemove"),
+    view: window,
+    detail: 0,
+    screenX: sx, 
+    screenY: sy,
+    clientX: cx, 
+    clientY: cy,
+    ctrlKey: false,
+    altKey: false,
+    shiftKey: false,
+    metaKey: false,
+    button: 0,
+    relatedTarget: undefined
+  };
+  if (typeof( document.createEvent ) == "function") {
+    evt = document.createEvent("MouseEvents");
+    evt.initMouseEvent(type, 
+      e.bubbles, e.cancelable, e.view, e.detail,
+      e.screenX, e.screenY, e.clientX, e.clientY,
+      e.ctrlKey, e.altKey, e.shiftKey, e.metaKey,
+      e.button, document.body.parentNode);
+  } else if (document.createEventObject) {
+    evt = document.createEventObject();
+    for (prop in e) {
+    evt[prop] = e[prop];
+  }
+    evt.button = { 0:1, 1:4, 2:2 }[evt.button] || evt.button;
+  }
+  return evt;
+}
+
+function dispatchEvent (el, evt) {
+  if (el.dispatchEvent) {
+    el.dispatchEvent(evt);
+  } else if (el.fireEvent) {
+    el.fireEvent('on' + type, evt);
+  }
+  return evt;
+}
